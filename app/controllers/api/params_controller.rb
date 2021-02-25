34fed87d37aa43325a475f1f1c788c5378cf0424
Zack Borges <zackartborges@gmail.com>
2021-02-25 15:30:58 -0500

    in progress of figuring out params

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,6 +1,10 @@
 class Api::ParamsController < ApplicationController
   def first_letter
-    @first_letter_a = params["message"]
+    @get_name = params["message"]
+    @first_letter_a = @get_name.split("")
+    if @first_letter_a[0] == "a"
+      @first_letter_a = "Hey! Your name starts with an A."
+    end
     render "first_letter.json.jb"
   end
 end
