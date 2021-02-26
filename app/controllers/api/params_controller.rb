bf6164d988762b937949e6640a96eac7e7c14b26
Zack Borges <zackartborges@gmail.com>
2021-02-26 15:05:15 -0500

    in progress of getting params  figured out

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,10 +1,22 @@
 class Api::ParamsController < ApplicationController
   def first_letter
-    @get_name = params["message"]
-    @first_letter_a = @get_name.split("")
-    if @first_letter_a[0] == "a"
-      @first_letter_a = "Hey! Your name starts with an A."
-    end
+    get_name = params["message"]
+
+    if get_name[0] == "a"
+      @message = "Hey! Your name starts with an A."
+    else @message = "#{get_name}"     end
     render "first_letter.json.jb"
+  end
+
+  def number_guess
+    number = 30
+    input_number = params[:your_guess].to_i
+    if input_number < 30
+      @message = "Too low!"
+    elsif input_number > 30
+      @message = "Too high"
+    else
+      @message = "You got it!"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/first_letter_a" => "params#first_letter"
+    get "/params_example/:your_guess" => "params#number_guess"
   end
 end
